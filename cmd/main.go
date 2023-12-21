b8546e69d55264e8d058c4dbe84791a01b2d0913
Antoha2 <102018866+Antoha2@users.noreply.github.com>
2023-12-21 11:47:14 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/staszigzag/sandbox/internal/config"
+
+func main() {
+	cfg := config.MustLoad()
+	// TODO: инициализировать объект конфига
+
+	// TODO: инициализировать логгер
+
+	// TODO: инициализировать приложение (app)
+
+	// TODO: запустить gRPC-сервер приложения
+
+}
